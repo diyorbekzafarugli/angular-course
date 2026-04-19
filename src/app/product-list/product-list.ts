cd16217b806c588577e2d779c44100923c610603
Diyorbek Boriboyev <diyorbekzafarugli@gmail.com>
2026-04-19 13:31:14 +0500

    add Bootstrap and Angular Material dependencies; implement navbar and product card components; enhance product list layout and styles

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -2,26 +2,18 @@ import { Component, inject, signal } from '@angular/core';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { MatCardModule } from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [MatCardModule, MatGridListModule],
   templateUrl: './product-list.html',
-  styleUrl: './product-list.scss',
+  styleUrls: ['./product-list.scss'], // 👈 SHU MUHIM
 })
 export class ProductList {
   products = signal<Product[]>([]);
   productService = inject(ProductService);
 
   productList = toSignal(this.productService.getProducts(), { initialValue: [] });
-  // constructor() {
-  //   this.productService.getProducts().subscribe({
-  //     next: (data) => {
-  //       this.products.set(data);
-  //     },
-  //     error: (err) => {
-  //       console.error('Error fetching products:', err);
-  //     }
-  //   });
-  // }
 }
